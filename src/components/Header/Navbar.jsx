dc08e3d2636aefe95876229a87bb8800ba0db838
nahid5766 <nahidurrahman6244@gmail.com>
2026-09-16 16:45:10 +0600

    navbar done

---
+++ src/components/Header/Navbar.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import logo from "../../assets/logo.png";
+import { FaGithub } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              aria-label="Menu"
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+          <ul
+            tabIndex={-1}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            <li>
+              <a>Home</a>
+            </li>
+            <li>
+              <a>Apps</a>
+            </li>
+            <li>
+              <a>Installation</a>
+            </li>
+          </ul>
+        </div>
+        <a className="btn btn-ghost text-xl">
+          {" "}
+          <img className="w-10" src={logo} alt="" />{" "}
+          <span className="bg-gradient-to-r from-[#632EE3] to-[#9F62F2] bg-clip-text text-transparent">
+            HERO.IO
+          </span>
+        </a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1 gap-8">
+          <li>
+            <a>Home</a>
+          </li>
+          <li>
+            <a>Apps</a>
+          </li>
+          <li>
+            <a>Installation</a>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <a className="btn text-white bg-gradient-to-r from-[#632EE3] to-[#9F62F2]">
+          <FaGithub /> Contribute
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/Root/Root.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../../components/Header/Navbar';
+
+const Root = () => {
+    return (
+        <div className='max-w-[1400px] mx-auto'>
+            <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default Root;
