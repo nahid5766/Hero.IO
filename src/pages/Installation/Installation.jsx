7486dbee8419fe5dd93b253e1f1bc8f698b1f839
nahid5766 <nahidurrahman6244@gmail.com>
2026-09-22 04:50:39 +0600

    error page done

--- src/pages/Installation/Installation.jsx
+++ src/pages/Installation/Installation.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
 
 const Installation = () => {
-    return (
-        <div>
-            <h2>Installation apps</h2>
-        </div>
-    );
+  return (
+    <div className="bg-[#D2D2D2]/30 min-h-screen">
+      <div className="py-10 text-center items-center justify-center">
+        <h2 className="text-4xl font-extrabold mt-15">Your Installed Apps</h2>
+        <p className="mt-4 text-gray-400">Explore All Trending Apps on the Market developed by us</p>
+      </div>
+
+      <div className="flex justify-between px-19">
+        <h1 className="text-2xl font-bold">01 App found</h1>
+        <button>sort</button>
+      </div>
+    </div>
+  );
 };
 
 export default Installation;
